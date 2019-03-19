4d73795e03a9970762fccef97b05bf812e23a3ff
Leonard Chen <wxclx98@qq.com>
2019-03-19 19:45:26 +0800

    Add readTrips function.

---
+++ Itinerary/Model/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by Leonard Chen on 3/19/19.
+//  Copyright © 2019 Leonard Chen. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [TripModel]()
+}
